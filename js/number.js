7ffa562d7e6890bd99794160f19b32199c849cc5
pavanchambhare <pavanchambhare88@gmail.com>
2022-04-25 08:21:04 +0530

    first commit

---
+++ js/number.js
@@ -0,0 +1,11 @@
+
+document.getElementById('btn') ;
+document.getElementById('demo').onclick = function changeName() {
+
+    document.getElementById('demo').textContent = "Help me";
+    document.getElementById('demo').style = "color:green";
+    document.getElementById('btn').style = "border: 2px solid green";
+    document.getElementById('pi').style = "border: 2px solid green";
+    document.getElementById('pi').style = "color: white";
+
+ }
